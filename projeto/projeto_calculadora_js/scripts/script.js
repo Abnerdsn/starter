4bcc44525ff276b30e4f6a7007caaad06e161ee2
Abner Santos <abner.dsn@gmail.com>
2023-01-14 22:08:50 -0300

    fim do projeto

--- projeto/projeto_calculadora_js/scripts/script.js
+++ projeto/projeto_calculadora_js/scripts/script.js
@@ -1,3 +1,34 @@
+// Get the button elements
+const lightModeButton = document.getElementById("light-mode-button");
+const darkModeButton = document.getElementById("dark-mode-button");
+
+// Add event listeners to the buttons
+lightModeButton.addEventListener("click", function() {
+  document.body.classList.remove("dark-mode");
+  document.body.classList.add("light-mode");
+});
+
+darkModeButton.addEventListener("click", function() {
+  document.body.classList.remove("light-mode");
+  document.body.classList.add("dark-mode");
+});
+
+lightModeButton.addEventListener("click", function() {
+    document.body.classList.remove("dark-mode");
+    document.body.classList.add("light-mode");
+    document.body.addEventListener("transitionend", function(){
+      document.body.classList.add("light-mode");
+    }, {once: true});
+  });
+  
+  darkModeButton.addEventListener("click", function() {
+    document.body.classList.remove("light-mode");
+    document.body.classList.add("dark-mode");
+    document.body.addEventListener("transitionend", function(){
+      document.body.classList.add("dark-mode");
+    }, {once: true});
+  });
+  
 function calcular(n1, n2){
 n1 = parseFloat(document.getElementById("n1").value);
 n2 = parseFloat(document.getElementById("n2").value);
