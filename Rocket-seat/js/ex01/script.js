74cb9b126167ca09a5054a8bb6ee753975e26ad7
Abner Santos <abner.dsn@gmail.com>
2023-01-14 21:35:00 -0300

    vai

---
+++ Rocket-seat/js/ex01/script.js
@@ -0,0 +1,7 @@
+let weight;
+console.log(typeof weight)
+let name = "Abner";
+let age = 34;
+let stars = 4.5;
+isSubscribed = true;
+
---
+++ projeto/projeto_calculadora_js/scripts/script.js
@@ -0,0 +1,27 @@
+function calcular(n1, n2){
+n1 = parseFloat(document.getElementById("n1").value);
+n2 = parseFloat(document.getElementById("n2").value);
+
+selector = document.getElementById("selector").value;
+
+switch(selector){
+    case '+':
+        calculo = (n1 + n2)
+        document.getElementById("resultado").innerHTML =`o resultado da soma de ${n1} e ${n2} = ${calculo}`
+        break
+        
+        case '-':
+        calculo = (n1 - n2)
+        document.getElementById("resultado").innerHTML =`o resultado da subtração de ${n1} e ${n2} = ${calculo}`
+        break
+        case '*':
+        calculo = (n1 * n2)
+        document.getElementById("resultado").innerHTML =`o resultado da mutiplicação de ${n1} e ${n2} = ${calculo}`
+        break
+        case '/':
+        calculo = (n1 / n2)
+        document.getElementById("resultado").innerHTML =`o resultado da divisão de ${n1} e ${n2} = ${calculo}`
+        break
+       
+}
+}
